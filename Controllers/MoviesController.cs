2e9de23b4c3b3f0595321530d661f46bb16865de
LEONARDO ROJAS VASILESCU <lrvae2020@gmail.com>
2020-06-11 21:09:20 -0500

    Cambios 2

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -46,7 +46,7 @@ namespace MovieApp.Controllers
             var result = await _movieRepository.AddMovie(model);
             if (result >= 1)
             {
-                return Ok(new { Message = "Movie added succesfully. cambio" });
+                return Ok(new { Message = "Movie added succesfully. cambio2" });
             }
 
             return StatusCode(500, new { Message = "Some error happened." });
